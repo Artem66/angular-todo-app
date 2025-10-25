382d164ed1e7c8faa6542b840b1d8aa07b818f8f
Artem66 <melodpi@gmail.com>
2025-10-25 13:46:39 +0100

    added basic login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+const todos = [
+  { id: '1', title: 'Learn Angular', completed: true },
+  { id: '2', title: 'Build a Todo App', completed: false },
+  { id: '3', title: 'Master Signals', completed: false },
+  { id: '4', title: 'Explore Standalone Components', completed: false },
+];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('angular-todo-app');
+  protected readonly title = signal('angular-todo-app')
+  todos = todos;
+  editing = false
 }
