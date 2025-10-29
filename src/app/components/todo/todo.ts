48792720baf6ea6d3643845257bd983cd815b5aa
Artem66 <melodpi@gmail.com>
2025-10-29 07:30:30 +0000

    added focusing

--- src/app/components/todo/todo.ts
+++ src/app/components/todo/todo.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, ElementRef, Input, Output, ViewChild } from '@angular/core';
 import { Todo as TodoType } from '../../types/todo';
 import { FormsModule } from '@angular/forms';
 import { EventEmitter } from '@angular/core';
@@ -13,6 +13,12 @@ export class Todo {
   @Output() delete = new EventEmitter();
 
   @Input() todo!: TodoType;
+
+  @ViewChild('titleField') set titleField(filed: ElementRef) {
+    if(filed) {
+      filed.nativeElement.focus();
+    }
+  };
 
   editing = false;
 }
