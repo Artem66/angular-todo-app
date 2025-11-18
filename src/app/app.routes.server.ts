b13fa6df653835d389a30e6aa3c869550e4b532e
Artem66 <melodpi@gmail.com>
2025-11-18 19:34:25 +0000

    added getPrerenderParams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,17 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'todos/:filterStatus',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      return Promise.resolve([
+        { filterStatus: 'all' },
+        { filterStatus: 'active' },
+        { filterStatus: 'completed' }
+      ]);
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
